af109e9716005711a9458eea41c743f681ba8309
dinesh-paladugula <dineshpaladugula2002@gmail.com>
2025-01-07 23:52:31 +0530

    exception handling

--- LiveWeatherData.py
+++ LiveWeatherData.py
@@ -5,6 +5,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
+# Exception Handling
 def weather(city):
     city = city.replace(" ", "+")
     url = f'https://www.google.com/search?q={city}+weather+in+celcius'
